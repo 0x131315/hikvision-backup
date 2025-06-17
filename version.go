c6f35eb20103c6483888ee84e9a93db34c84ca93
0x131315 <2824664+0x131315@users.noreply.github.com>
2025-06-17 14:51:25 +0300

    ref print version

---
+++ version.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+// Эти переменные будут "заполнены" во время сборки
+var (
+	version   = "development" // Версия сборки
+	commit    = "none"        // Хэш коммита
+	buildDate = "unknown"     // Дата сборки
+)
+
+func printVersion() {
+	fmt.Printf("Version:   %s\n", version)
+	fmt.Printf("Commit:    %s\n", commit)
+	fmt.Printf("BuildDate: %s\n", buildDate)
+	os.Exit(0)
+}
